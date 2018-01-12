726892f1fbcaf550b9b3e6550ad07be91a1a79cd
Tema <amg1999@yandex.ru>
2018-01-12 03:14:35 +0300

    2.0.4 update

--- src/main/java/ftblag/stoneblockutilities/jei/JeiPlugin.java
+++ src/main/java/ftblag/stoneblockutilities/jei/JeiPlugin.java
@@ -2,6 +2,8 @@ package ftblag.stoneblockutilities.jei;
 
 import com.google.common.collect.Lists;
 
+import ftblag.stoneblockutilities.gui.ContainerWB;
+import ftblag.stoneblockutilities.gui.GuiWB;
 import ftblag.stoneblockutilities.jei.crook.CrookRecipe;
 import ftblag.stoneblockutilities.jei.crook.CrookRecipeCategory;
 import ftblag.stoneblockutilities.jei.hand.HandRecipe;
@@ -32,5 +34,7 @@ public class JeiPlugin implements IModPlugin {
         registry.addRecipes(Lists.newArrayList(new HandRecipe()), HandRecipeCategory.UID);
         registry.addRecipeCatalyst(new ItemStack(SBURegistry.hand), HandRecipeCategory.UID);
         registry.getJeiHelpers().getIngredientBlacklist().addIngredientToBlacklist(new ItemStack(SBURegistry.hand));
+        registry.addRecipeClickArea(GuiWB.class, 88, 32, 28, 23, VanillaRecipeCategoryUid.CRAFTING);
+        registry.getRecipeTransferRegistry().addRecipeTransferHandler(ContainerWB.class, VanillaRecipeCategoryUid.CRAFTING, 1, 9, 10, 36);
     }
 }
